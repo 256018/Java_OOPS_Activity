a208ca9ea431ba11dbb9b944241b78d3520fecbe
Rohan Gupta <47993921+256018@users.noreply.github.com>
2021-06-04 15:41:46 +0530

    Add files via upload

---
+++ Activity3/ShapeMain.java
@@ -0,0 +1,46 @@
+import java.util.*;
+abstract class Shape
+{
+    int value;
+    abstract public void calculateArea(int value);
+}
+class Circle extends Shape
+{
+    public void calculateArea(int value)
+    {
+        System.out.println("Area of Circle is: " + (double)(Math.PI)*(value*value));
+    }
+}
+class Square extends Shape
+{
+    public void calculateArea(int value)
+    {
+        System.out.println("Area of Square is: " + (value*value));
+    }
+}
+public class ShapeMain 
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Type Circle to calculate area of circle");
+        System.out.println("Type Square to calculate area of square");
+        System.out.println("Enter your choice");
+        String choice = sc.next();
+        if(choice.equals("Circle"))
+        {
+            Circle object = new Circle();
+            System.out.println("Enter value");
+            int value = sc.nextInt();
+            object.calculateArea(value);
+        }
+        else
+        {
+            Square object = new Square();
+            System.out.println("Enter value");
+            int value = sc.nextInt();
+            object.calculateArea(value);
+        }
+        sc.close();
+    }
+}
